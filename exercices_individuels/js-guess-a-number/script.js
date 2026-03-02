9f69aa0a13506aba3f45db9e37554e2f5da67c37
Elodie S <elodie.sevestre86@gmail.com>
2026-03-02 11:30:08 +0100

    ex-guess-a-number

--- exercices_individuels/js-guess-a-number/script.js
+++ exercices_individuels/js-guess-a-number/script.js
@@ -1,28 +1,49 @@
-// ETAPE 1
+// === Etape 1 ===
 
-function askNumber() {
-  const givenNumber = Number(prompt("Give me a number please :"));
+// fonction qui demande un nombre à l'utilisateur
+// prompt() va collecter numberUser
+// stocker réponse dans variable
+
+function getAnumber() {
+  const message = prompt("Entre un nombre :");
+  const givenNumber = Number(message);
   return givenNumber;
 }
 
-// ETAPE 2
+// === Etape 2 ===
+
+// ex : nombre à deviner = 22;
+
+// fonction didIWin() avec givenNumber comme paramètre
+// si n < 22 -> "Plus grand"
+// si n > 22 -> "Plus petit"
+// si n = 22 -> "Bravo"
+// attention prompt() bloque exécution page et stoppe affichage
+// pour afficher des infos à la suite d'un prompt() -> alert()
+
+let number = 22;
 
 function didIWin(givenNumber) {
-  if (givenNumber === 22) {
-    alert("Bravo !!! Vous avez deviné !");
-  } else if (givenNumber > 22) {
-    alert("Plus petit ");
-  } else {
-    alert("Plus grand ");
+  while (givenNumber !== number) {
+    if (givenNumber < number) {
+      alert("Plus grand");
+    } else {
+      alert("Plus petit");
+    }
+    givenNumber = getAnumber();
   }
+  alert("Bravo");
 }
 
+// fonction gamePlay() qui gèrera :
+// appel de fonction
+// transmission variable d'une fonction à l'autre
+
 function gamePlay() {
-  let win = false;
-  while (win !== true) {
-    let givenNumber = askNumber();
-    win = didIWin(givenNumber);
-  }
+  let userNumber = getAnumber();
+  didIWin(userNumber);
 }
 
 gamePlay();
+
+// === Etape 3 ===
